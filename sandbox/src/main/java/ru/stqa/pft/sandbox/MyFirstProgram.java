f709b1cd73b1416c9974005e9aa8bc72e55dc6ac
mugurel3000_ook <mugurel3000@gmail.com>
2020-01-06 19:02:04 +0200

    Created package for MyFirstProgram

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.sandbox;
+
+/* Name it "Hello.java"
+ * Compile it by running "javac Hello.java"
+ * Run it by typing "java Hello"
+ */
+ class MyFirstProgram {
+	public static void main(String args[])	{ System.out.println("Hello, world!");
+	}
+}
+
+
