fdc0c8acd9ba0d4b5b15976977480f95774a2e69
mugurel3000_ook <mugurel3000@gmail.com>
2020-01-06 21:26:49 +0200

    Added some features

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,8 +4,40 @@ package ru.stqa.pft.sandbox;
  * Compile it by running "javac Hello.java"
  * Run it by typing "java Hello"
  */
- class MyFirstProgram {
-	public static void main(String args[])	{ System.out.println("Hello, world!");
+class MyFirstProgram {
+	public static void main(String args[]) {
+		/*norok ("world");
+		norok ("MAX");
+		norok ("Vasia");
+		matem("5");*/
+		double l = 5;
+		System.out.println("площ квдрата" + l + "=" + area(l));
+
+		double a = 5;
+		double b = a;
+		System.out.println("площ прям.угл" + a + "и" + b + "=" + area(a, b));
+
+	/*String some = "world";
+  System.out.println("Hello, " + some + "!");
+  int max = 5;
+  int s = max * max;
+  System.out.println("Площадь кв со сторн " + max + " = " + s);
+
+
+	public static void norok (String some) {
+		System.out.println("Hello," + some +"!");
+	}
+
+	public static void matem (String sis)	 {
+		System.out.println(5 + sis);
+	}
+	 */
+	}
+	public  static double area(double lin) {
+		return lin * lin ;
+	}
+	public  static double area(double a, double b  ) {
+		return a* b;
 	}
 }
 
