0d6c620e961c2c120d185984f7148855b11cca37
mugurel3000_ook <mugurel3000@gmail.com>
2020-01-12 13:42:27 +0200

    program Distance_2 / home work

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Distance_2.java
@@ -0,0 +1,23 @@
+package ru.stqa.pft.sandbox;
+
+public class Distance_2 {
+  public static void main(String a[]) {
+    Point point1 = new Point(3, 8);
+    Point point2 = new Point(3, 8);
+    System.out.println("Дистанция между точками А и B = " + point1.distance(point1, point2));
+  }
+}
+
+class Point {
+  int a;
+  int b;
+
+  public Point(int a, int b) {
+    this.a = a;
+    this.b = b;
+  }
+
+  public double distance(Point p1, Point p2) {
+    return Math.sqrt((p2.b - p2.a) * (p2.b - p2.a) + (p1.b - p1.a) * (p1.b - p1.a));
+  }
+}
