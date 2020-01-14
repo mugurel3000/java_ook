3cf9bb9da63e700c1a24d5dc2dbc9ccf0bb01814
mugurel3000_ook <mugurel3000@gmail.com>
2020-01-14 13:19:14 +0200

    Test Point home work #3

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
@@ -5,14 +5,14 @@ import org.testng.annotations.Test;
 
 public class PointTest {
   @Test
-  public void testArea() {
+  public void testArea1() {
     Point point1 = new Point(3, 8);
     Point point2 = new Point(3, 8);
     Assert.assertEquals(point1.distance(point2), 7.0710678118654755);
   }
 
   @Test
-  public void testArea1() {
+  public void testArea2() {
     Point point1 = new Point(3, 8);
     Point point2 = new Point(3, 8);
     point1.a = 9;
@@ -23,7 +23,7 @@ public class PointTest {
   }
 
   @Test
-  public void testArea2() {
+  public void testArea3() {
     Point point1 = new Point(3, 8);
     Point point2 = new Point(3, 8);
     point1.a = 5;
